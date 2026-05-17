d74501fa5ce77a026820de9fb28dfd0ad1ba6b93
e9kia <gattam035@gmail.com>
2026-05-17 23:25:52 +0300

    Ali Jitam ❤️: Database Connection Pooler Active & Secure

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,15 +6,15 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma_v2: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma_v2 ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma_v2 = prisma;
 }
